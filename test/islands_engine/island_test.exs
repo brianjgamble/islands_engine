9fe5f01809336c1c0493c7744697360b22b1bd0a
Brian Gamble <cc@theclearcoder.com>
2018-10-22 21:40:09 -0500

    Determine if islands overlap

--- test/islands_engine/island_test.exs
+++ test/islands_engine/island_test.exs
@@ -25,4 +25,38 @@ defmodule IslandsEngine.IslandTest do
 
     assert {:error, :invalid_coordinate} = Island.new(:l_shape, coordinate)
   end
+
+  defp square_island do
+    {:ok, square_coordinate} = Coordinate.new(1, 1)
+    {:ok, square} = Island.new(:square, square_coordinate)
+    square
+  end
+
+  defp dot_island do
+    {:ok, dot_coordinate} = Coordinate.new(1, 2)
+    {:ok, dot} = Island.new(:dot, dot_coordinate)
+    dot
+  end
+
+  defp l_shape_island do
+    {:ok, l_shape_coordinate} = Coordinate.new(5, 5)
+    {:ok, l_shape} = Island.new(:l_shape, l_shape_coordinate)
+    l_shape
+  end
+
+  test "overlaps?/2 returns true when islands overlap" do
+    square = square_island
+    dot    = dot_island
+
+    assert Island.overlaps?(square, dot)
+  end
+
+  test "overlaps?/2 returns false when islands don't overlap" do
+    square  = square_island
+    dot     = dot_island
+    l_shape = l_shape_island
+
+    refute Island.overlaps?(square, l_shape)
+    refute Island.overlaps?(dot, l_shape)
+  end
 end
