3ee658b700c60a2700f857621d27ff9e79801ffe
Brian Gamble <cc@theclearcoder.com>
2018-10-24 12:23:46 -0500

    Handle positioning island and ensuring all islands are positioned

--- test/islands_engine/board_test.exs
+++ test/islands_engine/board_test.exs
@@ -1,12 +1,42 @@
 defmodule IslandsEngine.BoardTest do
   use ExUnit.Case, async: true
 
-  alias IslandsEngine.Board
+  alias IslandsEngine.{Board, Coordinate, Island}
+
+  def square_island do
+    {:ok, square_coordinate} = Coordinate.new(1, 1)
+    {:ok, square} = Island.new(:square, square_coordinate)
+    square
+  end
+
+  def dot_island do
+    {:ok, dot_coordinate} = Coordinate.new(2, 2)
+    {:ok, dot} = Island.new(:dot, dot_coordinate)
+    dot
+  end
 
   test "new/0 create an empty map" do
-    map = Board.new()
+    board = Board.new()
+    assert Enum.empty?(Map.keys(board))
+  end
 
-    assert is_map(map)
-    assert Enum.empty?(map)
+  test "position_island/3 allows an island to be placed on the board" do
+    board =
+      Board.new
+      |> Board.position_island(:square, square_island())
+
+    placed_island = board.square
+    {:ok, first_coordinate} = Coordinate.new(1, 1)
+
+    assert MapSet.member?(placed_island.coordinates, first_coordinate)
+  end
+
+  test "position_island/3 prevents islands from overlapping" do
+    board =
+      Board.new
+      |> Board.position_island(:square, square_island())
+
+    assert Board.position_island(board, :dot, dot_island()) ==
+      {:error, :overlapping_island}
   end
 end
