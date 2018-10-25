550620945d1b02c9ce97b3e78052c7cc182c07aa
Brian Gamble <cc@theclearcoder.com>
2018-10-25 11:24:54 -0500

    Add new state :players_set

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -4,4 +4,10 @@ defmodule IslandsEngine.Rules do
   defstruct state: :initialized
 
   def new(), do: %Rules{}
+
+  def check(%Rules{state: :initialized} = rules, :add_player) do
+    {:ok, %Rules{rules | state: :players_set}}
+  end
+  
+  def check(_state, _action), do: :error
 end
--- test/islands_engine/rules_test.exs
+++ test/islands_engine/rules_test.exs
@@ -3,8 +3,24 @@ defmodule IslandsEngine.RulesTest do
 
   alias IslandsEngine.Rules
 
-  test "new/0 creates a new struct" do
+  test "new/0 returns an initialized struct" do
     rules = Rules.new()
     assert rules.state == :initialized
+  end
+
+  test "check/2 adding a player changes the state" do
+    {:ok, rules} =
+      Rules.new()
+      |> Rules.check(:add_player)
+
+    assert rules.state == :players_set
+  end
+
+  test "check/2 return an error with the wrong action" do
+    result =
+      Rules.new()
+      |> Rules.check(:completely_wrong_action)
+
+    assert result == :error
   end
 end
